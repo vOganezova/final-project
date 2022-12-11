206169a605f0727127fcb3a2b7d1c1fdc48567cf
NinoPotskhoraia <victoriaoganezova1@gmail.com>
2022-12-11 15:15:45 +0400

    finished markup, added container styles and flex, added filter menu

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
 //hamburger menu
-const navToggler = document.querySelector('.hamburger-menu');
-const closeNav = document.querySelector('.close');
-const navList = document.querySelector('.nav-list');
+const nav = document.getElementById('navBar');
+const toggler = document.querySelector('.nav-toggler');
 
-navToggler.addEventListener('click', ()=>{
-    if (navList.style.display == 'none') {
-        navList.style.display = 'block';
-        navToggler.style.display = 'none';
-        closeNav.style.display = 'block';
-    } else {
-        navList.style.display = 'none';
-        closeNav.style.display = 'none';
-        navToggler.style.display = 'block';
-    }
-});
-
-closeNav.addEventListener('click', ()=>{
-    if(navList.style.display == 'block'){
-       navList.style.display = 'none';
-       navToggler.style.display = 'block';
-       closeNav.style.display = 'none';
-    } else{
-        navList.style.display = 'block';
-        closeNav.style.display = 'block';
-        navToggler.style.display = 'none';
-    }
+toggler.addEventListener('click', ()=>{
+nav.classList.toggle('show');
+toggler.classList.toggle('change');
+userContainer.classList.remove('show');
 })
+
+
 
 //get user
 
@@ -33,6 +16,10 @@ closeNav.addEventListener('click', ()=>{
 const userContainer = document.getElementById("user");
 const userInfo = document.getElementById("userInfo");
 const showProfile = document.getElementById("openAccount");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+
 
 function getUser(){
     fetch("https://jsonplaceholder.typicode.com/users", {method:"GET"},)
@@ -74,10 +61,307 @@ function getUser(){
 
 getUser();
 
-showProfile.addEventListener('click', ()=>{
+showProfile.addEventListener("click", function () {
+    modal.classList.add("open-modal");
+  });
+  closeBtn.addEventListener("click", function () {
+    modal.classList.remove("open-modal");
+  });
+
+
+
+//slider
+
+const prevBtn = document.getElementById("arrow-left");
+const nextBtn = document.getElementById("arrow-right");
+const content = document.getElementById("slider-content");
+const wrapper = document.querySelector('.slider-wrapper');
+let current = 0;
+
+let data = [
+    {   
+        index:0,
+        text:'whatever',
+        desc:'blablablablaaaaaa',
+        img: "./assets/pot.jpg"
+    },
+    {
+        index:1,
+        text:'what',
+        desc:'blablablablaaaaaa',
+        img: "./assets/pot.jpg"
+    },
+    {
+        index:2,
+        text:'ever',
+        desc:'blablablablaaaaaa',
+        img: "./assets/pot.jpg"
+    }
+]
+
+
+const createSliderContainer = () => {
+    const container = document.createElement("div");
+    container.classList.add("slider-container");
+  
+  
+    return container;
+  }
+  
+  const addImg = (obj)=>{
+     const imgDiv = document.createElement('div');
+     imgDiv.style.backgroundImage = `url(${obj.img})`;
+     imgDiv.classList.add('bg');
+     console.log(imgDiv);
+     console.log(obj.img);
+       
+     return imgDiv;
+  }
+
+  const addBtn = ()=>{
+    const sliderBtn = document.createElement('button');
+    sliderBtn.innerText = "Find out More";
+    sliderBtn.classList.add("slider-btn");
+    return sliderBtn;
+  }
+  
+  
+  const addText = (obj)=>{
+  const text = document.createElement('h2');
+ 
+  text.innerText = obj.text;
+  text.classList.add("slider-title");
+  return text;
+  }
+
+  const addDesc = (obj)=>{
+    const desc = document.createElement('p');
+   
+    desc.innerText = obj.desc;
+    desc.classList.add("slider-desc");
+    return desc;
+    }
+  
+  const addSliderNav = ()=> {
+      const dots = document.createElement("div");
+      dots.classList.add("dots");
     
-    userContainer.classList.toggle('show');
+      data.forEach((obj) => {
+        const dot = document.createElement("div");
+        dot.classList.add("dot");
+        dot.innerHTML = '<i class="fa-solid fa-circle"></i>';
+        dots.appendChild(dot);
+      });
+  
+     
     
+      return dots;
+  }
+  
+  const dotsNav = ()=>{
+      const dotsArr = Array.from(document.querySelectorAll('.fa-circle'));
+      dotsArr.forEach((dot, index)=>{
+          if(index === current){
+              dot.style.color = 'white';
+          }
+              dot.addEventListener('click', ()=>{
+                  current = index;
+                  slide();
+              })
+         
+      })
+     
+  }
+  
+  const slide = () => {
+      content.innerHTML = " ";
+      const slide = createSliderContainer();
+      const bgDiv = addImg(data[current]);
+      const text = addText(data[current]);
+      const desc = addDesc(data[current]);
+      const btn = addBtn();
+      const dots = addSliderNav();
+
+      const sliderText = document.createElement('div');
+      sliderText.appendChild(text);
+      sliderText.appendChild(desc);
+      sliderText.appendChild(btn);
+      sliderText.classList.add("slider-text");
+      
+  
+      bgDiv.appendChild(sliderText);
+      slide.appendChild(bgDiv);
+      content.appendChild(slide);
+      content.appendChild(dots);
+      dotsNav();
+    }
+  
+    nextBtn.addEventListener('click', ()=>{
+      if (current === data.length - 1) {
+          current = 0;
+          slide();
+          return;
+        }
+        current++;
+        slide();
+        dotsNav();
+    });
+  
+    prevBtn.addEventListener('click', ()=>{
+      if (current === 0) {
+          current = data.length - 1;
+          slide();
+          return;
+        }
+        current--;
+        slide();
+        dotsNav();
+    });
+  
+    slide();
+    dotsNav();
+  
+//menu
+const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/item-1.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/item-2.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/item-3.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/item-7.jpeg",
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/item-8.jpeg",
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+  ];
+  
+
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+window.addEventListener("DOMContentLoaded", function () {
+  diplayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join("");
+
+
+  sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
     })
+    .join("");
 
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+  console.log(filterBtns);
 
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
+      }
+    });
+  });
+}
